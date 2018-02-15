cd258f2b7d75643161f1bceb0545c7657a4b2732
pablofsf <ablocorrea@hotmail.com>
2018-02-15 11:14:05 +0100

    Examples

--- src/data/Arff_Reader.java
+++ src/data/Arff_Reader.java
@@ -1,8 +1,13 @@
 package data;
 
 import java.nio.file.Path;
+import java.util.Enumeration;
+
 import weka.core.converters.ConverterUtils.DataSource;
+import weka.core.Attribute;
 import weka.core.Instances;
+import weka.core.Instance;
+
 
 public class Arff_Reader {
 	
@@ -24,8 +29,7 @@ public class Arff_Reader {
 		try {
 			DataSource examplesSource = new DataSource(path);
 			this.examples = examplesSource.getDataSet();
-			
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -35,11 +39,21 @@ public class Arff_Reader {
 		System.out.println(this.examples);
 	}
 	
-	public static int main(String[] args){
-		Arff_Reader lector = new Arff_Reader("./wait.arff");
+	public void remove(){
+		Instance example1 = examples.instance(0);
+		double[] values3 = examples.attributeToDoubleArray(0);
+		//System.out.print(values3[0].intValue());
+		double[] values = example1.toDoubleArray();
+		System.out.println(examples.attribute("Price").value(examples.attribute("Price").numValues()-1));
+		//Attribute wait1 = example1.attributeSparse(5);
+		Enumeration jaja = examples.instance(0).enumerateAttributes();
+		System.out.print(values);
+	}
+	
+	public static void main(String[] args){
+		Arff_Reader lector = new Arff_Reader("/home/pablo/data/Informatica/IDTrees/src/data/wait.arff");
 		
+		//lector.remove();
 		lector.print();
-		
-		return 0;
 	}
 }
