5fde639acc7f52a507c4dd097c702b96c9095a8a
pablofsf <ablocorrea@hotmail.com>
2018-02-17 16:48:40 +0100

    Basic Attribute class created

---
+++ src/data/Attribute.java
@@ -0,0 +1,31 @@
+package data;
+
+import java.util.ArrayList;
+
+public class Attribute {
+
+	String name;
+	ArrayList<String> values;
+	
+	public Attribute(String name, ArrayList<String> values){
+		this.name = name;
+		this.values = values;
+	}
+	
+	public Attribute(String name){
+		this.name = name;
+		this.values = new ArrayList<String>(0);
+	}
+	
+	public int numValues(){
+		return values.size();
+	}
+	
+	public void addValue(String value){
+		this.values.add(value);
+	}
+	
+	public ArrayList<String> getValues(){
+		return new ArrayList<String>(values);
+	}
+}
