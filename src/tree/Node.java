440f0f771bbc25980af8f41d73ef8cb230cf0ce8
Jaime Saura Bastida <Jaime Saura Bastida>
2018-02-21 11:02:53 +0000

    Tree

--- src/tree/Node.java
+++ src/tree/Node.java
@@ -1,22 +1,29 @@
+package src*;
+
 public class Node {
 
     private int value;
+    private Atribute x;
 
     private Node father;
     private Node left;
     private Node right;
 
-    public Node(int value) {
+    public Node(int value, Atribute x) {
         this.value = value;
+        this.x = x;
     }
 
     public void setValue(int value) {
         this.value = value;
     }
+    public void setX(Atribute x){this.x = x;}
 
     public int getValue() {
         return value;
     }
+
+    public void getX(Atribute x){return x;}
 
     public Node getFather() {
         return father;
